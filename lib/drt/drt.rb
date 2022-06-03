87f8b501ccd0666be0c7f3d289b85b49eacb1086
mash-97 <mahimnhd97@gmail.com>
2022-06-03 22:53:05 +0600

    format and consistency

--- lib/drt/drt.rb
+++ lib/drt/drt.rb
@@ -3,82 +3,76 @@ require 'yaml'
 require 'faraday'
 require 'active_record'
 
-module DRT 
+module DRT
+  DRT_DIRPATH = File.join(ENV['HOME'], '.drt')
+  DRT_CONFIGPATH = File.join(DRT_DIRPATH, 'drt_config.yml')
+  DRT_LOGPATH = File.join(DRT_DIRPATH, 'drt.log')
 
-    DRT_DIRPATH = File.join(ENV["HOME"], ".drt")
-    DRT_CONFIGPATH = File.join(DRT_DIRPATH, "drt_config.yml")
-    DRT_LOGPATH = File.join(DRT_DIRPATH, "drt.log")
-	
-	LOGGER_GEN = ->(log_path=DRT_LOGPATH) do
-		L = Logger.new(log_path)
-		L.formatter = Proc.new{
-			|severity, datetime, progname, msg|
-			datetime = datetime.strftime("%d-%m-%Y %H:%M:%S")
-			if progname then
-				"[#{datetime}]: (#{severity}) (#{progname}) => #{msg}" 
-			else
-				"[#{datetime}]: (#{severity}) => #{msg}" 
-			end
-		}
-	end
-	
-	SITE_URL = "http://software.diu.edu.bd:8189"
-	FARADAY_CONNECTION_GEN = ->() do
-		Faraday.new(
-				url: DRT::SITE_URL,
-				headers: {
-					'Content-Type' => "application/json"
-				}
-			){ |faraday|
-				faraday.request :json
-				faraday.response :json
-				
-			}
-	end
-	
-	DB_CONNECTION_GEN = ->(db_path, log_path) do 
-		ActiveRecord::Base.establish_connection(adapter: :sqlite3, database: db_path, logger: Logger.new(log_path))
-	end
-	
-	
-    class Config
-		attr_accessor :db_path
-		attr_accessor :db_log_path
-		
-		def initialize(config_path = DRT_CONFIGPATH)
-			@config_path = config_path
-			@configs = Config.loadConfigs(@config_path)
-			@db_path, @db_log_path = Config.parseConfigs(@configs)
-		end
-		
-		def self.parseConfigs(configs_hash)
-			db_path, db_log_path = [configs_hash["db_path"], configs_hash["db_log_path"]]
-			return [db_path, db_log_path]
-		end
-		
-		def self.loadConfigs(config_path)
-			return YAML.load_file(config_path)
-		end
-	end
-	
-    class DRT 
-
-        def initialize(config, logger = DRT::LOGGER_GEN.call())
-			@faraday_connection = DRT::FARADAY_CONNECTION_GEN.call()
-			@config = config
-			@logger = logger
-			@db = DRT::DB_CONNECTION_GEN.call(@config.db_path, @config.db_log_path)
-		end
-		
-		
+  LOGGER_GEN = lambda do |log_path = DRT_LOGPATH|
+    l = Logger.new(log_path)
+    l.formatter = proc do |severity, datetime, progname, msg|
+      datetime = datetime.strftime('%d-%m-%Y %H:%M:%S')
+      if progname
+        "[#{datetime}]: (#{severity}) (#{progname}) => #{msg}"
+      else
+        "[#{datetime}]: (#{severity}) => #{msg}"
+      end
     end
-	def requestStudentInfo(faraday_connection=DRT::FARADAY_CONNECTION_GEN.call(), student_id)
-		response = faraday_connection.get("result/studentInfo", studentId: student_id)
-		return response.body
-	end
-	
-	def requestStudentResult(faraday_connection=DRT::FARADAY_CONNECTION_GEN.call(), student_id, semester_id)
-		response = faraday_connection.get("result", studentId: student_id, semesterId: semester_id)
-		return response.body
-	end
+  end
+
+  SITE_URL = 'http://software.diu.edu.bd:8189'.freeze
+  FARADAY_CONNECTION_GEN = lambda do
+    Faraday.new(
+      url: SITE_URL,
+      headers: {
+        'Content-Type' => 'application/json'
+      }
+    ) do |faraday|
+      faraday.request :json
+      faraday.response :json
+    end
+  end
+
+  DB_CONNECTION_GEN = lambda do |db_path, log_path|
+    ActiveRecord::Base.establish_connection(adapter: :sqlite3, database: db_path, logger: Logger.new(log_path))
+  end
+
+  class Config
+    attr_accessor :db_path, :db_log_path
+
+    def initialize(config_path = DRT_CONFIGPATH)
+      @config_path = config_path
+      @configs = Config.load_configs(@config_path)
+      @db_path, @db_log_path = Config.parse_configs(@configs)
+    end
+
+    def self.parse_configs(configs_hash)
+      db_path = configs_hash['db_path']
+      db_log_path = configs_hash['db_log_path']
+      [db_path, db_log_path]
+    end
+
+    def self.load_configs(config_path)
+      YAML.load_file(config_path)
+    end
+  end
+
+  class DRT
+    def initialize(config, logger = DRT::LOGGER_GEN.call())
+      @faraday_connection = DRT::FARADAY_CONNECTION_GEN.call
+      @config = config
+      @logger = logger
+      @db = DRT::DB_CONNECTION_GEN.call(@config.db_path, @config.db_log_path)
+    end
+  end
+
+  def self.request_student_info(student_id, faraday_connection = FARADAY_CONNECTION_GEN.call())
+    response = faraday_connection.get('result/studentInfo', studentId: student_id)
+    response.body
+  end
+
+  def self.request_student_result(student_id, semester_id, faraday_connection = FARADAY_CONNECTION_GEN.call())
+    response = faraday_connection.get('result', studentId: student_id, semesterId: semester_id)
+    response.body
+  end
 end
--- test/drt_test.rb
+++ test/drt_test.rb
@@ -1,78 +1,80 @@
 # frozen_string_literal: true
 
-require "test_helper"
-
-include DRT
+require 'test_helper'
 
 # ENSURE test/tmp dir for test purpose
-TEST_TMP_PATH = File.join(File.absolute_path(File.dirname(__FILE__)), "tmp")
-unless(Dir.exists?(TEST_TMP_PATH)) then
-	Dir.mkdir(TEST_TMP_PATH)
-	File.new(File.join(TEST_TMP_PATH, "test.tst"), "w+").close()
+TEST_TMP_PATH = File.join(File.absolute_path(File.dirname(__FILE__)), 'tmp')
+unless Dir.exist?(TEST_TMP_PATH)
+  Dir.mkdir(TEST_TMP_PATH)
+  File.new(File.join(TEST_TMP_PATH, 'test.tst'), 'w+').close
+end
+
+class C < ActiveRecord::Migration[5.2]
+  def up
+    puts('==> C UP: Migration Attempt')
+    unless table_exists?(:tests)
+      puts('Creating tests table!')
+      create_table(:tests, id: false) do |table|
+        table.column(:test_id, :string, primary_key: true)
+        table.column(:title, :string, unique: true)
+      end
+    end
+
+    unless table_exists?(:associated_tests)
+      puts('Creating associated_tests table!')
+      create_table(:associated_tests, id: false) do |table|
+        table.column(:at_id, :string, primary_key: true)
+        table.column(:name, :string)
+        table.column(:test_id, :string)
+      end
+    end
+
+    if column_exists?(:associated_tests,
+                      :test_id) &&	column_exists?(:tests,
+                                                  :test_id) && !foreign_key_exists?(:associated_tests, :tests)
+      add_foreign_key(
+        :associated_tests,
+        :tests,
+        column: :test_id,
+        primary_key: :test_id
+      )
+    end
+  end
 end
 
 class DrtTest < Test::Unit::TestCase
-  test "VERSION" do
+  include DRT
+
+  test 'VERSION' do
     assert do
       ::Drt.const_defined?(:VERSION)
     end
   end
 
-  test "something useful" do
-    assert_equal("3", "3")
+  test 'something useful' do
+    assert_equal('3', '3')
   end
-  
-  test "Test requesting student info" do
-	return
-	si = DRT::requestStudentInfo("181-15-955")
-	assert(DRT.public_methods.include?(:requestStudentInfo))
-  end 
-  test "Test requesting student result" do
-	return
-	si = DRT::requestStudentResult("181-15-955", 221)
-	assert(DRT.public_methods.include?(:requestStudentResult))
-  end 
-  
-  test "Test Database Connection Through Active Record" do 
-	db_path = File.join(TEST_TMP_PATH, "test.db")
-	log_path = File.join(TEST_TMP_PATH, "test.log")
-	assert(DRT::DB_CONNECTION_GEN.call(db_path, log_path))
-	class C < ActiveRecord::Migration[5.2]
-		def up() 
-			unless table_exists?(:tests) then
-				create_table(:tests, id: false) do
-					|table|
-					table.column(:test_id, :string, primary_key: true)
-					table.column(:title, :string, unique: true)
-				end 
-			end
-			
-			unless table_exists?(:associated_tests) then
-				create_table(:associated_tests, id: false) do
-					|table|
-					table.column(:at_id, :string, primary_key: true)
-					table.column(:name, :string)
-					table.column(:test_id, :string)
-				end 
-			end
-			
-			if (column_exists?(:associated_tests, :test_id) and	column_exists?(:tests, :test_id) and not foreign_key_exists?(:associated_tests, :tests)) then
-				add_foreign_key(
-					:associated_tests, 
-					:tests, 
-					column: :test_id, 
-					primary_key: :test_id
-				) 
-			end
-			
-		end
-	end
-	connection = ActiveRecord::Base.connection
-	C.migrate(:up)
-	assert(ActiveRecord::Base.connection.tables.length >= 2)
-	assert((connection.column_exists?(:associated_tests, :test_id) and connection.column_exists?(:tests, :test_id)))
-	assert(connection.foreign_key_exists?(:associated_tests, :tests))
+
+  test 'Test requesting student info' do
+    si = ::DRT.request_student_info('181-15-955')
+    assert(si['studentName'], 'Mashiur Rahman')
+    assert(::DRT.public_methods.include?(:request_student_info))
   end
-  
-  
+  test 'Test requesting student result' do
+    sr = ::DRT.request_student_result('181-15-955', 221)
+    assert_equal(sr.length, 5)
+    assert(::DRT.public_methods.include?(:request_student_result))
+  end
+
+  test 'Test Database Connection Through Active Record' do
+    db_path = File.join(TEST_TMP_PATH, 'test.db')
+    log_path = File.join(TEST_TMP_PATH, 'test.log')
+    assert(DB_CONNECTION_GEN.call(db_path, log_path))
+
+    connection = ActiveRecord::Base.connection
+    C.migrate(:up)
+    assert(ActiveRecord::Base.connection.tables.length >= 2)
+    assert((connection.column_exists?(:associated_tests, :test_id) and connection.column_exists?(:tests, :test_id)))
+    assert(connection.foreign_key_exists?(:associated_tests, :tests))
+  end
 end
