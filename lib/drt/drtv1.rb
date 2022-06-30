26ece2f752c5d6442f5a8e19a0affcd42f4d9a48
mash-97 <mahimnhd97@gmail.com>
2022-07-01 03:38:38 +0600

    random fixes&doc

--- lib/drt/drtv1.rb
+++ lib/drt/drtv1.rb
@@ -14,21 +14,19 @@ module DRT1
   # module to maintain structure
   DRT_DIRPATH = File.join(ENV['HOME'], '.drt')
   DRT_CONFIGPATH = File.join(DRT_DIRPATH, 'drt_config.yml')
-  DRT_LOGPATH = ->{File.join(DRT_DIRPATH, "drt__#{Time.now.strftime('%d_%m_%Y__%H_%M_%S')}.log")}
+  DRT_LOGPATH = -> { File.join(DRT_DIRPATH, "drt__#{Time.now.strftime('%d_%m_%Y__%H_%M_%S')}.log") }
 
   DEFAULT_DB_PATH = File.join(DRT_DIRPATH, 'drt.db')
   DEFAULT_DB_LOG_PATH = File.join(DRT_DIRPATH, 'drt_db.log')
 
   class LogDev < Logger::LogDevice
-	def write(data, &puts_lambda)
+    def write(data, &puts_lambda)
       super(data)
-      if puts_lambda then
-	    data = puts_lambda
-	  end
-	  puts(data)
-	end
+      data = puts_lambda if puts_lambda
+      puts(data)
+    end
   end
-  
+
   LOGGER_GEN = lambda do |log_path = DRT_LOGPATH.call()|
     l = Logger.new(LogDev(log_path))
     l.formatter = proc do |severity, datetime, progname, msg|
@@ -129,10 +127,8 @@ module DRT1
     ##
     # This class does the heavy work
     attr_accessor :config
-    attr_accessor :logger
-    attr_accessor :db
-    attr_accessor :faraday_connection
-    
+    attr_accessor :logger, :db, :faraday_connection
+
     def initialize(config, logger = LOGGER_GEN.call())
       @faraday_connection = FARADAY_CONNECTION_GEN.call
       @config = config
@@ -142,69 +138,60 @@ module DRT1
         DRTMigrator.migrate(:up)
       end
     end
-
   end
 
-
-  def self.get_student_info(student_id, logger=nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
+  # get student info by calling faraday connection
+  # returns nil if not found
+  # returns parsed student data if found
+  # will throw exceptions if any problem faced during connection call
+  def self.get_student_info(student_id, logger = nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
     logger.info("(get_student_info:request) #{student_id}") if logger
-    
     response = faraday_connection.get('result/studentInfo', studentId: student_id)
-    
-	logger.info("(get_student_info:response) #{student_id} #{response.status}") if logger
-    
+    logger.info("(get_student_info:response) #{student_id} #{response.status}") if logger
     parsed_student_info = parse_student_info(response.body)
-
     if parsed_student_info[:student_id].nil?
-		logger.info("(get_student_info:result) #{student_id} [invalid]") if logger
-		return nil
-	end
-	logger.info("(get_student_info:result) #{student_id} [valid]") if logger
-    return parsed_student_info
+      logger.info("(get_student_info:result) #{student_id} [invalid]") if logger
+      return nil
+    end
+    logger.info("(get_student_info:result) #{student_id} [valid]") if logger
+    parsed_student_info
   end
 
-
-  def self.get_semester_result(student_id, semester_id, logger=nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
+  # get semester result by the faraday connection
+  # requires student_id and semester_id
+  # returns semester results for the courses taken
+  # returns nil if not found
+  # will throw exception if problem faced during connection call
+  def self.get_semester_result(student_id, semester_id, logger = nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
     logger.info("(get_semester_result:request) #{student_id}:#{semester_id}") if logger
-    
-    response = faraday_connection.get('result', studentId: student_id)
-    
-	logger.info("(get_semester_result:response) #{student_id}:#{semester_id} #{response.status}") if logger
-    
+    response = faraday_connection.get('result', studentId: student_id, semesterId: semester_id)
+    logger.info("(get_semester_result:response) #{student_id}:#{semester_id} #{response.status}") if logger
     parsed_semester_result = parse_semester_result(response.body)
-
-    if parsed_semester_result.length = 0 then
-		logger.info("(get_semester_result:result) #{student_id}:#{semester_id} [invalid]") if logger
-		return nil
-	end
-	logger.info("(get_semester_result:result) #{student_id}:#{semester_id} [valid]") if logger
-    return parsed_semester_result
+    if parsed_semester_result.length = 0
+      logger.info("(get_semester_result:result) #{student_id}:#{semester_id} [invalid]") if logger
+      return nil
+    end
+    logger.info("(get_semester_result:result) #{student_id}:#{semester_id} [valid]") if logger
+    parsed_semester_result
   end
-  
 
   # updates a single student info given student_id and faraday_connection
-  def self.update_student_info(student_id, logger=nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
+  def self.update_student_info(student_id, logger = nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
     student_info = get_student_info(student_id, logger, faraday_connection)
-    
-    if not student_info then
-	  logger.info("(update_student_info) #{student_id} [failed to get info, aborting update]") if logger
-	  return nil
-	end
-    
+    unless student_info
+      logger.info("(update_student_info) #{student_id} [failed to get info, aborting update]") if logger
+      return nil
+    end
     student = Student.find_by(student_id: student_info[:student_id])
-
     if student
-      
       student.update(**student_info)
-      
       logger.info("(update_student_info) #{student_id} [found in db and updated]") if logger
-      
-      return student
+      student
     else
       begin
         student = Student.create(**student_info)
         logger.info("(update_student_info) #{student_id} [new insert in db]") if logger
-        return student
+        student
       rescue StandardError => e
         logger.error("(update_student_info) #{student_id} [#{e}]") if logger
         raise StudentNull.new(student_id, e)
@@ -212,26 +199,27 @@ module DRT1
     end
   end
 
-
   # updates a single semester_result given student_id, semester_id and faraday_connection
-  def self.update_semester_result(student_id, semester_id, logger=nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
-    
+  def self.update_semester_result(student_id, semester_id, logger = nil, faraday_connection = FARADAY_CONNECTION_GEN.call())
     student = Student.find_by(student_id: student_id)
-    
-    if not student then
+
+    unless student
       logger.info("(update_semester_result) #{student_id}:#{semester_id} [not found in db]") if logger
       student = update_student_info(student_id, logger, faraday_connection)
-      if not student then
-        logger.warning("(update_semester_result) #{student_id}:#{semester_id} [could not find student info, aborting]") if logger
+      unless student
+        if logger
+          logger.warning("(update_semester_result) #{student_id}:#{semester_id} [could not find student info, aborting]")
+        end
         return nil
       end
     end
-    
-    
+
     semester_result = get_semester_result(student_id, semester_id, logger, faraday_connection)
-    
-    if not semester_result then
-      logger.warning("(update_semester_result) #{student_id}:#{semester_id} [no semester result found, aborting]") if logger
+
+    unless semester_result
+      if logger
+        logger.warning("(update_semester_result) #{student_id}:#{semester_id} [no semester result found, aborting]")
+      end
       return nil
     end
 
@@ -241,16 +229,22 @@ module DRT1
       tssr = student.semester_results.find_by(semester_id: ssr[:semester_id], course_id: ssr[:course_id])
       if tssr
         tssr.update(**ssr)
-        logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{ssr[:course_id]}:#{ssr[:course_title]} found in db, updated]") if logger
+        if logger
+          logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{ssr[:course_id]}:#{ssr[:course_title]} found in db, updated]")
+        end
       else
         tssr = SemesterResult.create(**ssr)
-        logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{ssr[:course_id]}:#{ssr[:course_title]} not found in db, created]") if logger
+        if logger
+          logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{ssr[:course_id]}:#{ssr[:course_title]} not found in db, created]")
+        end
         new_insertion += 1
       end
       updated_semester_result << tssr
     end
 
-    logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{new_insertion} created, #{updated_semester_result.length - new_insertion} updated]") if logger
-    return updated_semester_result
+    if logger
+      logger.info("(update_semester_result) #{student_id}:#{semester_id} [#{new_insertion} created, #{updated_semester_result.length - new_insertion} updated]")
+    end
+    updated_semester_result
   end
 end
